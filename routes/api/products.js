5e366bfe3dbe216430eb2c1559560327c4cda619
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-05-01 18:03:48 -0500

    anatomy of an API RESTful and crud implementation

---
+++ routes/api/products.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+const productMocks = require('../../utils/mocks/products');
+
+router.get("/", function (req, res) {
+  const { query } = req.query;
+
+  res.status(200).json({
+    data: productMocks,
+    message: "product listed",
+  });
+});
+
+router.get("/:productId", function (req, res) {
+  const { productId } = req.params;
+
+  res.status(200).json({
+    data: productMocks[0],
+    message: "product retrieved",
+  });
+});
+
+router.post("/", function (req, res) {
+  res.status(201).json({
+    data: productMocks[0],
+    message: "product listed",
+  });
+});
+
+router.put("/:productId", function (req, res) {
+  res.status(200).json({
+    data: productMocks,
+    message: "product updated",
+  });
+});
+
+router.delete("/:productId", function (req, res) {
+  res.status(200).json({
+    data: productMocks[0],
+    message: "product deleted",
+  });
+});
+
+module.exports = router;
