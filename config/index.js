b73948ab964261b88b27fe6c2ef69326b3831d8f
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-05-03 01:51:58 -0500

    setting and using passport

--- config/index.js
+++ config/index.js
@@ -8,6 +8,10 @@ const config = {
   dbHost: process.env.DB_HOST,
   dbPort: process.env.DB_PORT,
   dbName: process.env.DB_NAME,
+  authAdminUsername: process.env.AUTH_ADMIN_USERNAME,
+  authAdminPassword: process.env.AUTH_ADMIN_PASSWORD,
+  authAdminEmail: process.env.AUTH_ADMIN_EMAIL,
+  authJwtSecret: process.env.AUTH_JWT_SECRET,
 };
 
 module.exports = { config };
